0f2090c69ba75d2ae6697854037656b784959dbf
brian_pos <brian_pos@hotmail.com>
2023-03-15 10:41:48 +1100

    Include the R5 endpoint! (at /fhir5)

---
+++ src/main/java/org/example/FhirService_R5.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import javax.servlet.ServletException;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FhirService_R5 extends RestfulServer {
+
+  public FhirService_R5() {
+    super(FhirContext.forR5());
+
+    registerProvider(new EvaluatorHAPI_R5());
+  }
+
+  @Override
+  protected void initialize() throws ServletException {
+    super.initialize();
+  }
+
+  @Bean
+  public ServletRegistrationBean fhirServletR5() {
+    return new ServletRegistrationBean(
+        new FhirService_R5(), "/fhir5/*");
+  }
+}
